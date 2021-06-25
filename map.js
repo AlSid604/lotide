1d26947e91ee933309468069139f4721cf10338c
AlSid604 <alex_sidor97@hotmail.com>
2021-06-25 07:27:37 +0000

    implemented take Until day 4

--- map.js
+++ map.js
@@ -1,4 +1,29 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (array1 === array2) {
+    return `✅✅✅ Passed, These arrays contain the same values`;
+  }
+  return `🛑🛑🛑 Failed, These arrays did not contain the same values`;
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const map = function (array, callback) {
   const results = [];
@@ -8,16 +33,6 @@ const map = function (array, callback) {
   return results;
 };
 
+const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, (word) => word[0]);
-console.log(results1);
-
-// const map = function() {
-// }
-
-const assertArraysEqual = function (array1, array2) {
-  if (array1 === array2) {
-    return `✅✅✅ Passed, These arrays contain the same values`;
-  }
-  return `🛑🛑🛑 Failed, These arrays did not contain the same values`;
-};
-
+console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "t"]));
