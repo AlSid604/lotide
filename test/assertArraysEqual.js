f6bfd039aadb7801837dccaa21cdf7f6c4760961
AlSid604 <alex_sidor97@hotmail.com>
2021-06-29 03:04:34 +0000

    Lots of new files for lots of new mocha and chai. Also changed my head function to work with chai/mocha

--- test/assertArraysEqual.js
+++
@@ -1,4 +0,0 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const eqArrays = require("../assertArraysEqual");
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
